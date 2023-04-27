7b2fbc3099a5cc36323231360f48aabfa10012c1
Jmat19 <47995875+Jmat19@users.noreply.github.com>
2023-04-27 10:48:47 -0400

    wall jump progress

--- Squirrel Platformer/Assets/Jump Tutorial/PlayerMovement.cs
+++ Squirrel Platformer/Assets/Jump Tutorial/PlayerMovement.cs
@@ -6,21 +6,39 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private float Move;
+    private float horizontal;
+
+    private bool isWallSliding;
+    private float wallSlideSpeed;
 
     public float jump;
+    //private int nroPulos = 2;
 
     private bool isGrounded;
     public Transform feetPos;
     public float checkRadius;
+    public Transform groundCheck;
     public LayerMask whatIsGround;
+    public Transform wallCheck;
+    public LayerMask wallLayer;
+   // public Transform wallCheck;
+   // public float wallCheckDistance;
+
+   // public float wallJumpForce;
+   // public Vector2 wallJumpDirection;
 
     private float jumpTimeCounter;
     public float jumpTime;
 
     private bool isJumping;
-    private bool isTouchingWall;
+    //private bool isTouchingWall;
+
+    /*public float startDash;
+    public float dashSpeed;
+    public float dashCooldown;*/
 
     private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,7 +56,7 @@ public class PlayerMovement : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
         //Debug.Log(isGrounded);
 
-        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+        //isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
 
         if(Move > 0)
         {
@@ -81,7 +99,55 @@ public class PlayerMovement : MonoBehaviour
             isJumping = false;
         }
 
-        //jumpTimeCounter -= Time.deltaTime;
+        WallSlide();
+
+        /*if (isWallSliding)
+        {
+            if (rb.velocity.y < -wallSlideSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -wallSlideSpeed);
+            }
+        }
+
+        if (nroPulos > 0 && !isWallSliding)
+        {
+            StopDash();
+            nroPulos--;
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jump);
+        }
+
+        else if (isWallSliding)
+        {
+            Vector2 force = new Vector2(wallJumpForce * wallJumpDirection.x * -facingDirection, wallJumpForce * wallJumpDirection.y);
+
+            rb.velocity = Vector2.zero;
+
+            rb.AddForce(force, ForceMode)
+        }*/
+    }
+
+    private bool CurGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, whatIsGround);
+    }
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallSlide()
+    {
+        if (IsWalled() && !CurGrounded() && horizontal != 0f)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -100,4 +166,28 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+   /* private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        if (facingRIght)
+        {
+            Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
+        }
+        else
+        {
+            Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x - wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
+        }
+    }
+
+    private void CheckWallSliding()
+    {
+        if(isTouchingWall && !isGrounded && rb.velocity.y < 0 && moveInput != 0)
+        {
+            isWallSliding = true;
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }*/
 }
